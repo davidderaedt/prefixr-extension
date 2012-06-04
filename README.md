5f884d223df15187be31954a2dcb8a2749b70332
dderaedt <dderaedt@adobe.com>
2012-06-04 14:27:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Brackets prefixr extension
+===
+
+A small brackets extension to replace the current CSS text selection with the prefixed result returned by Prefixr.
+
+Prefixr is a service created by Jeffrey Way (from nettuts). More information on the service:
+http://prefixr.com/
+
+To install this extension, simply place the ```prefixr``` folder inside the ```brackets/src/extensions/user``` folder, and reload Brackets.
+
+
+Usage
+=====
+Select some CSS code, and then select "Prefixr" from the Edit menu. Your code should then be replaced by the prefixed equivalent.
+
+
+Known issues
+=====
+
+The returned Prefixr code does not include any  indentation.
+
+No feedback is given to the user while waiting for the service response.
---
+++ main.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+ 
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+
+/** extension to generate CSS prefixes using the Prefixr API */
+define(function (require, exports, module) {
+    'use strict';
+
+
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        EditorManager  = brackets.getModule("editor/EditorManager"),
+        Menus          = brackets.getModule("command/Menus");
+
+
+    var ERROR_MSG = "Unable to use Prefixr";
+    var EMPTY_MSG = "Empty selection";
+    var API_URL = "http://prefixr.com/api/index.php";
+    var MY_COMMAND_ID = "prefixr.prefixr";
+    var MENU_NAME = "Prefixr";
+
+    
+    function handleAction() {
+  
+        // Retrieve selection
+        var selectedText = EditorManager.getFocusedEditor().getSelectedText();
+
+        if(selectedText==""){
+            window.alert(EMPTY_MSG);
+            return;
+        }
+
+        
+        console.log("sending to prefixr");
+
+        // Send to prefixr
+        $.ajax({
+            url: API_URL,
+            type:"GET",
+            data:"css="+selectedText,
+
+            error:function(data){
+                window.alert(ERROR_MSG);
+            },
+
+            success:function(data){
+                EditorManager.getFocusedEditor()._codeMirror.replaceSelection(data);
+            }
+        });
+
+    }
+
+    // Register the command and insert in the Edit menu
+    CommandManager.register(MENU_NAME, MY_COMMAND_ID, handleAction);
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuItem("menu-prefixr-prefixr", MY_COMMAND_ID);
+    
+});
